8b5d3558ffc4cf738359d07a6600e32d0d3c4857
UDAY VARA <udayvara26@gmail.com>
2024-02-03 14:47:10 +0530

    server.js Updated

--- Backend/server.js
+++ Backend/server.js
@@ -13,14 +13,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.json()); // to parse the incoming request with JSON payload  ( from req.body)
 app.use(cookieParser()); // to parse the incoming request with
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
-app.use("/api/users",userRoutes);
-
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();

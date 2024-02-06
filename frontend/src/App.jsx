e4fd4de96f8d43935a7600a23680b2508db56e54
UDAY VARA <udayvara26@gmail.com>
2024-02-06 21:53:15 +0530

    updated piece of code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,20 @@ import "./App.css";
 function App() {
   return (
     <>
+    <button className="btn">Button</button>
+    <button className="btn btn-neutral">Neutral</button>
+    <button className="btn btn-primary">Primary</button>
+    <button className="btn btn-secondary">Secondary</button>
+    <button className="btn btn-accent">Accent</button>
+    <button className="btn btn-ghost">Ghost</button>
+    <button className="btn btn-link">Link</button>
     </>
   );
 }
 
 export default App;
+
+// npm i react-scripts@latest
+
+//npm install -D tailwindcss
+//npx tailwindcss init
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [require("daisyui")],
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};

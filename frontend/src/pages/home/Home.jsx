ad4f233e03378f582a75e118ce5926d242393e2f
UDAY VARA <udayvara26@gmail.com>
2024-02-10 13:50:25 +0530

    updated piece of code

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
-import Sidebar from "../../componentes/sidebar/sidebar"
+import Sidebar from "../../componentes/sidebar/sidebar";
 
 const Home = () => {
   return (
-    <div className=" flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 
-        bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0"> 
-    <Sidebar /> 
+    <div
+      className="flex sm:h - [450px] md: h-[550px] rounded-lg overflow-hidden bg-gray-400 
+         bg-clip-padding backdrop backdrop-filter backdrop-blur-lg bg-opacity-0"
+    >
+      <Sidebar />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

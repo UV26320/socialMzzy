c6d17f3423d1098b6e5a83463f17b04b0c0a5bb4
UDAY VARA <udayvara26@gmail.com>
2024-02-11 17:56:56 +0530

    Message Input Added

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 import Messages from "./Messages";
+import MessageInput from "./MessageInput";
 
 const MessageContainer = () => {
   return (
@@ -12,7 +13,7 @@ const MessageContainer = () => {
         </div>
 
         <Messages />
-        {/* <MessageInput /> */}
+        <MessageInput />/
       </>
     </div>
   );

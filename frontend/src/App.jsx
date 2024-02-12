ef41f88479e4b6e09e53a9ec616a6047948d1bfb
UDAY VARA <udayvara26@gmail.com>
2024-02-12 20:32:54 +0530

    Toaster modual Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,15 @@ import "./App.css";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SingUp";
 import Home from "./pages/home/Home";
-import { Toaster } from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="p-4 h-screen flex item-center justify-center">
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
       <Toaster />
     </div>

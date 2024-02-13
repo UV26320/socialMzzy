3872cc550b97c96385bbe51a9c8dd8aec88d1533
UDAY VARA <udayvara26@gmail.com>
2024-02-13 18:27:32 +0530

    Authuser functionality Added

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -18,3 +18,15 @@ export const AuthContextProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+/*
+
+This code is part of a React application and implements an authentication context using the
+ React Context API. It provides a context named AuthContext to manage authentication-related 
+ state. The code includes a custom hook useAuthContext for easily consuming this context in 
+ functional components. Additionally, it defines a AuthContextProvider component, which 
+ wraps the application's components and initializes the authentication state using local storage. 
+ The provider makes the authentication state and a corresponding setter function accessible to its
+  descendant components through the AuthContext.
+
+  */
